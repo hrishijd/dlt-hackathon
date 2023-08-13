a3c10a37560a3185a62b3baefdd2f118e6c63f59
hrishijd <hrishi.jadhav24@gmail.com>
2023-08-14 04:13:06 +0530

    feat: Add script to deploy smart contract

--- smart-contracts/scripts/deploy.ts
+++ smart-contracts/scripts/deploy.ts
@@ -1,7 +1,10 @@
 import { ethers } from "hardhat";
 
 async function main() {
-
+    const vaultFactory = await ethers.getContractFactory("Vault");
+    const signers = await ethers.getSigners();
+    const vaultContract = await vaultFactory.connect(signers[0]).deploy();
+    console.log(await vaultContract.getAddress());
 }
 
 main().catch((error) => {
