893adfd25a4f42667322623da8f5f100109c7df3
hrishijd <hrishi.jadhav24@gmail.com>
2023-08-14 04:15:23 +0530

    feat: Update config to support hedera mainnet and testnet

--- smart-contracts/hardhat.config.ts
+++ smart-contracts/hardhat.config.ts
@@ -1,8 +1,19 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
-
+import {config as dotConfig} from "dotenv";
+dotConfig();
 const config: HardhatUserConfig = {
   solidity: "0.8.19",
+  networks: {
+    hederaTestnet:{
+        url: `https://pool.arkhia.io/hedera/testnet/json-rpc/v1/${process.env.ARKHIA_KEY}`,
+        accounts: [`${process.env.PRIVATE_KEY}`],
+    },
+    hederaMainnet:{
+        url: `https://pool.arkhia.io/hedera/mainnet/json-rpc/v1/${process.env.ARKHIA_KEY}`,
+        accounts: [`${process.env.PRIVATE_KEY}`]
+    },
+  },
 };
 
 export default config;
