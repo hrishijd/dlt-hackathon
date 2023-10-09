de955fb25247b3f27963e8be809f0fd85fe93f7f
hrishijd <hrishi.jadhav24@gmail.com>
2023-10-09 10:45:04 +0530

    refactor: Refactor vault contract to: - Use Ownable by OZ - Improve timestamp logic

--- smart-contracts/contracts/Vault.sol
+++ smart-contracts/contracts/Vault.sol
@@ -1,34 +1,36 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.9;
 
+
 import {SafeERC20, IERC20} from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
+
 
 /**
  * @title Vault Contract
  * @dev A contract for managing deposits, supplying tokens, and monitoring balances.
  */
-contract Vault {
+contract Vault is Ownable{
     struct DepositInfo {
         address token;
+        uint32 splitTime;
+        uint32 lastSupplyTime;
         uint256 totalAmount;
         uint256 supplyAmount;
         uint256 minAmount;
-        uint256 splitTime;
-        uint256 lastSupplyTime;
-        address receiver;
+        address supplier;
     }
-
-    address internal constant owner = 0x50370f3BFE032Af808ac5e605cA6dEd64cbfB6E5;
 
     mapping(address => DepositInfo) public deposit;
 
-    event TokensSupplied(address indexed from, DepositInfo depositInfo);
+    event TokensSupplied(address indexed receiver, DepositInfo depositInfo);
 
-    event TokensDeposited(address indexed from, DepositInfo depositInfo);
+    event TokensDeposited(address indexed receiver, DepositInfo depositInfo);
 
-    modifier onlyOwner() {
-        require(msg.sender == owner, "Only the owner can call this function");
-        _;
+    event TokensWithdrawn(address indexed receiver, DepositInfo depositInfo);
+
+    constructor(address owner) Ownable(){
+        _transferOwnership(owner);
     }
 
     /**
@@ -41,22 +43,24 @@ contract Vault {
      * @param lastSupplyTime Timestamp of the last supply.
      * @param receiver Address to receive funds when supplying tokens.
      */
-    function addDepositInfo(
+    function depositTokens(
         address token,
+        uint32 splitTime,
+        uint32 lastSupplyTime,
         uint256 totalAmount,
         uint256 supplyAmount,
         uint256 minAmount,
-        uint256 splitTime,
-        uint256 lastSupplyTime,
-        address receiver
+        address receiver,
+        address supplier
     ) external payable {
-        require(deposit[msg.sender].totalAmount == 0, "Old balance should be removed before depositing new token");
-        if (token == address(0)) {
+        require(deposit[receiver].totalAmount == 0, "Old balance should be removed before depositing new token");
+        require(receiver != address(0), "receiver can't be address zero");
+        if(token == address(0)) {
             require(msg.value == totalAmount, "Value should be same as amount mentioned");
         } else {
             SafeERC20.safeTransferFrom(IERC20(token), msg.sender, address(this), totalAmount);
         }
-        deposit[msg.sender] = DepositInfo(token, totalAmount, supplyAmount, minAmount, splitTime, lastSupplyTime, receiver);
+        deposit[receiver] = DepositInfo(token, splitTime, lastSupplyTime, totalAmount, supplyAmount, minAmount, supplier);
         emit TokensDeposited(msg.sender, deposit[msg.sender]);
     }
 
@@ -64,12 +68,11 @@ contract Vault {
      * @dev Refills tokens in the contract.
      * @param amount Amount of tokens to refill.
      */
-    function refillTokens(uint256 amount) external payable {
-        DepositInfo memory depositInfo = deposit[msg.sender];
-        require(depositInfo.receiver != address(0), "Deposit info must be registered before refilling");
-        require(depositInfo.totalAmount == 0, "Old balance should be removed before depositing new token");
+    function refillTokens(address receiver, uint256 amount) external payable {
+        DepositInfo memory depositInfo = deposit[receiver];
+        require(receiver != address(0), "VAMOS_VAULT: Deposit info must be registered before refilling");
         if (depositInfo.token == address(0)) {
-            require(msg.value == amount, "Value should be same as amount mentioned");
+            require(msg.value == amount, "VAMOS_VAULT: Value should be same as amount mentioned");
         } else {
             SafeERC20.safeTransferFrom(IERC20(depositInfo.token), msg.sender, address(this), amount);
         }
@@ -95,31 +98,31 @@ contract Vault {
 
     /**
      * @dev Supplies tokens to an address.
-     * @param supplier Address of the account to supply tokens to.
-     * @param timestamp Timestamp of the supply.
+     * @param receiver Address of the account to supply tokens to.
      */
-    function supplyTokens(address supplier, uint256 timestamp) external {
-        DepositInfo memory depositInfo = deposit[supplier];
-        require(msg.sender == owner, "Sender should be owner");
-        require(depositInfo.totalAmount >= depositInfo.supplyAmount, "Not enough balance");
+    function supplyTokens(address receiver) external onlyOwner{
+        DepositInfo memory depositInfo = deposit[receiver];
+        require(depositInfo.totalAmount >= depositInfo.supplyAmount, "VAMOS_VAULT: Not enough balance for this receiver");
         depositInfo.totalAmount -= depositInfo.supplyAmount;
-        depositInfo.lastSupplyTime = timestamp;
-        deposit[supplier] = depositInfo;
-        _supply(depositInfo.token, depositInfo.receiver, depositInfo.supplyAmount);
-        emit TokensSupplied(supplier, depositInfo);
+        depositInfo.lastSupplyTime = uint32(block.timestamp);
+        deposit[receiver] = depositInfo;
+        _supply(depositInfo.token, receiver, depositInfo.supplyAmount);
+        emit TokensSupplied(receiver, depositInfo);
     }
 
     /**
      * @dev Withdraws the balance of tokens or Ether to a specified address.
+     * @param receiver Address that was registered to recieve tokens.
      * @param to Receiver of the tokens or Ether.
      */
-    function withrawBalance(address to) external {
-        DepositInfo memory depositInfo = deposit[msg.sender];
-        require(depositInfo.totalAmount > 0, "Not enough balance");
+    function withrawBalance(address receiver, address to) external {
+        DepositInfo memory depositInfo = deposit[receiver];
+        require(msg.sender == depositInfo.supplier, "VAMOS_VAULT: Can only be withdrawn by supplier");
+        require(depositInfo.totalAmount > 0, "VAMOS_VAULT: Not enough balance");
         uint256 amount = depositInfo.totalAmount;
         depositInfo.totalAmount = 0;
         delete deposit[msg.sender];
         _supply(depositInfo.token, to, amount);
-        emit TokensSupplied(msg.sender, depositInfo);
+        emit TokensWithdrawn(msg.sender, depositInfo);
     }
 }
